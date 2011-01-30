9c5b378e54ce20f07f21eefe5c45221f4c044293
Ian McCracken <ian@zenoss.com>
2011-01-30 08:40:52 -0600

    Some setup.py stuff

--- setup.py
+++ setup.py
@@ -5,22 +5,23 @@ version = '0.1'
 
 setup(name='pyxdeco',
       version=version,
-      description="",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      description="Extraordinary Decorators",
+      long_description=open('README.rst').read(),
+      klassifiers=[
+          "Programming Language :: Python",
+          "License :: OSI Approved :: MIT License",
+          "Intended Audience :: Developers"
+          "Topic :: Software Development :: Libraries :: Python Modules"
+      ],
       keywords='',
       author='Ian McCracken',
       author_email='ian.mccracken@gmail.com',
-      url='',
+      url='http://github.com/iancmcc/pyxdeco',
       license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
+      zip_safe=True,
+      install_requires=[],
       entry_points="""
-      # -*- Entry points: -*-
       """,
       )
